12e94ca3a4c66604c5d8627891e7bb0dceda8385
Wu Yongzheng <wuyongzheng@gmail.com>
2007-04-05 12:12:44 +0000

    more path in front; use FIELF_SEP as field separator

--- userapp.c
+++ userapp.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include "kucomm.h"
 
+#define FIELD_SEP "\t"
+
 typedef enum _FILE_INFORMATION_CLASS {
 	FileDirectoryInformation         = 1,
 	FileFullDirectoryInformation,   // 2
@@ -63,53 +65,69 @@ void process_event (const struct event *event)
 	if (event->type == ET_IGNORE)
 		return;
 
-	printf("%u %I64u %5d %5d %s ", event->serial, event->time.QuadPart, event->pid, event->tid, get_ntstatus_name(event->status));
+	printf("%u" FIELD_SEP "%I64u" FIELD_SEP "%5d" FIELD_SEP "%5d" FIELD_SEP "%s" FIELD_SEP,
+			event->serial,
+			event->time.QuadPart,
+			event->pid,
+			event->tid,
+			get_ntstatus_name(event->status));
 
 	switch (event->type) {
 	case ET_FILE_CREATE:
-		printf("create: access=%x share=%x attr=%x cd=%x co=%x \"%S\"\n",
-				event->file_create.desired_access, event->file_create.share_mode, event->file_create.attributes, event->file_create.creation_disposition, event->file_create.create_options,
-				event->path);
+		printf("create:" FIELD_SEP "\"%S\"" FIELD_SEP "access=0x%x" FIELD_SEP "share=0x%x" FIELD_SEP "attr=0x%x" FIELD_SEP "cd=0x%x" FIELD_SEP "co=0x%x\n",
+				event->path,
+				event->file_create.desired_access,
+				event->file_create.share_mode,
+				event->file_create.attributes,
+				event->file_create.creation_disposition,
+				event->file_create.create_options);
 		break;
 	case ET_FILE_CLOSE:
-		printf("close: \"%S\"\n", event->path);
+		printf("close:" FIELD_SEP "\"%S\"\n", event->path);
 		break;
 	case ET_FILE_READ:
-		printf("read: %I64u+%lu \"%S\"\n", event->file_rw.offset.QuadPart, event->file_rw.length, event->path);
+		printf("read:" FIELD_SEP "\"%S\"" FIELD_SEP "%I64u+%lu\n",
+				event->path,
+				event->file_rw.offset.QuadPart,
+				event->file_rw.length);
 		break;
 	case ET_FILE_WRITE:
-		printf("write: %I64u+%lu \"%S\"\n", event->file_rw.offset.QuadPart, event->file_rw.length, event->path);
+		printf("write:" FIELD_SEP "\"%S\"" FIELD_SEP "%I64u+%lu\n",
+				event->path,
+				event->file_rw.offset.QuadPart,
+				event->file_rw.length);
 		break;
 	case ET_FILE_CREATE_MAILSLOT:
-		printf("mslot: \"%S\"\n", event->path);
+		printf("mslot:" FIELD_SEP "\"%S\"\n", event->path);
 		break;
 	case ET_FILE_CREATE_NAMED_PIPE:
-		printf("pipe: \"%S\"\n", event->path);
+		printf("pipe:" FIELD_SEP "\"%S\"\n", event->path);
 		break;
 	case ET_FILE_QUERY_INFORMATION:
 		switch (event->file_info.info_type) {
-		case FileAllInformation: // TODO
-			printf("queryinfo: t=FileAllInformation s=%d \"%S\"\n",
-					event->file_info.info_size,
-					event->path);
+		case FileAllInformation:
+			printf("queryinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileAllInformation" FIELD_SEP "s=%d\n",
+					event->path,
+					event->file_info.info_size);
 			break;
 		case FileAttributeTagInformation:
-			printf("queryinfo: t=FileAttributeTagInformation attr=0x%x tag=0x%x \"%S\"\n",
+			printf("queryinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileAttributeTagInformation" FIELD_SEP "attr=0x%x" FIELD_SEP "tag=0x%x\n",
+					event->path,
 					event->file_info.info_data.file_info_attribute_tag.file_attributes,
-					event->file_info.info_data.file_info_attribute_tag.reparse_tag,
-					event->path);
+					event->file_info.info_data.file_info_attribute_tag.reparse_tag);
 			break;
 		case FileBasicInformation:
-			printf("queryinfo: t=FileBasicInformation ct=%I64u lat=%I64u lwt=%I64u lct=%I64u attr=0x%x \"%S\"\n",
+			printf("queryinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileBasicInformation" FIELD_SEP "ct=%I64u" FIELD_SEP "lat=%I64u" FIELD_SEP "lwt=%I64u" FIELD_SEP "lct=%I64u" FIELD_SEP "attr=0x%x\n",
+					event->path,
 					event->file_info.info_data.file_info_basic.creation_time,
 					event->file_info.info_data.file_info_basic.last_access_time,
 					event->file_info.info_data.file_info_basic.last_write_time,
 					event->file_info.info_data.file_info_basic.change_time,
-					event->file_info.info_data.file_info_basic.file_attributes,
-					event->path);
+					event->file_info.info_data.file_info_basic.file_attributes);
 			break;
 		case FileCompressionInformation:
-			printf("queryinfo: t=FileCompressionInformation size=%I64u format=%d unit=%d chunk=%d cluster=%d reserved=%d,%d,%d \"%S\"\n",
+			printf("queryinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileCompressionInformation" FIELD_SEP "size=%I64u" FIELD_SEP "format=%d" FIELD_SEP "unit=%d" FIELD_SEP "chunk=%d" FIELD_SEP "cluster=%d" FIELD_SEP "reserved=%d,%d,%d\n",
+					event->path,
 					event->file_info.info_data.file_info_compression.compressed_file_size,
 					event->file_info.info_data.file_info_compression.compression_format,
 					event->file_info.info_data.file_info_compression.compression_unit_shift,
@@ -117,173 +135,210 @@ void process_event (const struct event *event)
 					event->file_info.info_data.file_info_compression.cluster_shift,
 					event->file_info.info_data.file_info_compression.reserved[0],
 					event->file_info.info_data.file_info_compression.reserved[1],
-					event->file_info.info_data.file_info_compression.reserved[2],
-					event->path);
+					event->file_info.info_data.file_info_compression.reserved[2]);
 			break;
 		case FileEaInformation:
-			printf("queryinfo: t=FileEaInformation size=%d \"%S\"\n",
-					event->file_info.info_data.file_info_ea.ea_size,
-					event->path);
+			printf("queryinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileEaInformation" FIELD_SEP "size=%d\n",
+					event->path,
+					event->file_info.info_data.file_info_ea.ea_size);
 			break;
 		case FileInternalInformation:
-			printf("queryinfo: t=FileInternalInformation index=%I64u \"%S\"\n",
-					event->file_info.info_data.file_info_internal.index_number,
-					event->path);
+			printf("queryinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileInternalInformation" FIELD_SEP "index=%I64u\n",
+					event->path,
+					event->file_info.info_data.file_info_internal.index_number);
 			break;
 		case FileNameInformation:
-			printf("queryinfo: t=FileNameInformation name=\"%S\" \"%S\"\n",
-					event->file_info.info_data.file_info_name.file_name,
-					event->path);
+			printf("queryinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileNameInformation" FIELD_SEP "name=\"%S\"\n",
+					event->path,
+					event->file_info.info_data.file_info_name.file_name);
 			break;
 		case FileNetworkOpenInformation:
-			printf("queryinfo: t=FileNetworkOpenInformation ct=%I64u lat=%I64u lwt=%I64u lct=%I64u as=%I64u eof=%I64u attr=0x%x \"%S\"\n",
+			printf("queryinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileNetworkOpenInformation" FIELD_SEP "ct=%I64u" FIELD_SEP "lat=%I64u" FIELD_SEP "lwt=%I64u" FIELD_SEP "lct=%I64u" FIELD_SEP "as=%I64u" FIELD_SEP "eof=%I64u" FIELD_SEP "attr=0x%x\n",
+					event->path,
 					event->file_info.info_data.file_info_network_open.creation_time,
 					event->file_info.info_data.file_info_network_open.last_access_time,
 					event->file_info.info_data.file_info_network_open.last_write_time,
 					event->file_info.info_data.file_info_network_open.change_time,
 					event->file_info.info_data.file_info_network_open.allocation_size,
 					event->file_info.info_data.file_info_network_open.end_of_file,
-					event->file_info.info_data.file_info_network_open.file_attributes,
-					event->path);
+					event->file_info.info_data.file_info_network_open.file_attributes);
 			break;
 		case FilePositionInformation:
-			printf("queryinfo: t=FilePositionInformation pos=%I64u \"%S\"\n",
-					event->file_info.info_data.file_info_position.current_byte_offset,
-					event->path);
+			printf("queryinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FilePositionInformation" FIELD_SEP "pos=%I64u\n",
+					event->path,
+					event->file_info.info_data.file_info_position.current_byte_offset);
 			break;
 		case FileStandardInformation:
-			printf("queryinfo: t=FileStandardInformation as=%I64u eof=%I64u links=%d delete=%s dir=%s \"%S\"\n",
+			printf("queryinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileStandardInformation" FIELD_SEP "as=%I64u" FIELD_SEP "eof=%I64u" FIELD_SEP "links=%d" FIELD_SEP "delete=%s" FIELD_SEP "dir=%s\n",
+					event->path,
 					event->file_info.info_data.file_info_standard.allocation_size,
 					event->file_info.info_data.file_info_standard.end_of_file,
 					event->file_info.info_data.file_info_standard.number_of_links,
 					event->file_info.info_data.file_info_standard.delete_pending ? "true" : "false",
-					event->file_info.info_data.file_info_standard.directory ? "true" : "false",
-					event->path);
+					event->file_info.info_data.file_info_standard.directory ? "true" : "false");
 			break;
 		case FileStreamInformation:
-			printf("queryinfo: t=FileStreamInformation next=%lu size=%I64u as=%I64u name=\"%S\" \"%S\"\n",
+			printf("queryinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileStreamInformation" FIELD_SEP "next=%lu" FIELD_SEP "size=%I64u" FIELD_SEP "as=%I64u" FIELD_SEP "name=\"%S\"\n",
+					event->path,
 					event->file_info.info_data.file_info_stream.next_entry_offset,
 					event->file_info.info_data.file_info_stream.stream_size,
 					event->file_info.info_data.file_info_stream.stream_allocation_size,
-					event->file_info.info_data.file_info_stream.stream_name,
-					event->path);
+					event->file_info.info_data.file_info_stream.stream_name);
 			break;
 		default:
-			printf("queryinfo: t=%d s=%d \"%S\"\n",
-					event->file_info.info_type, event->file_info.info_size, event->path);
+			printf("queryinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=%d" FIELD_SEP "s=%d\n",
+					event->path,
+					event->file_info.info_type,
+					event->file_info.info_size);
 		}
 		break;
 	case ET_FILE_SET_INFORMATION:
 		switch (event->file_info.info_type) {
 		case FileAllocationInformation:
-			printf("setinfo: t=FileAllocationInformation AllocationSize=%I64u \"%S\"\n",
-					event->file_info.info_data.file_info_allocation.allocation_size,
-					event->path);
+			printf("setinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileAllocationInformation" FIELD_SEP "AllocationSize=%I64u\n",
+					event->path,
+					event->file_info.info_data.file_info_allocation.allocation_size);
 			break;
 		case FileBasicInformation:
-			printf("setinfo: t=FileBasicInformation ct=%I64u lat=%I64u lwt=%I64u lct=%I64u attr=0x%x \"%S\"\n",
+			printf("setinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileBasicInformation" FIELD_SEP "ct=%I64u" FIELD_SEP "lat=%I64u" FIELD_SEP "lwt=%I64u" FIELD_SEP "lct=%I64u" FIELD_SEP "attr=0x%x\n",
+					event->path,
 					event->file_info.info_data.file_info_basic.creation_time,
 					event->file_info.info_data.file_info_basic.last_access_time,
 					event->file_info.info_data.file_info_basic.last_write_time,
 					event->file_info.info_data.file_info_basic.change_time,
-					event->file_info.info_data.file_info_basic.file_attributes,
-					event->path);
+					event->file_info.info_data.file_info_basic.file_attributes);
 			break;
 		case FileDispositionInformation:
-			printf("setinfo: t=FileDispositionInformation delete=%s \"%S\"\n",
-					event->file_info.info_data.file_info_disposition.delete_file ? "true" : "false",
-					event->path);
+			printf("setinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileDispositionInformation" FIELD_SEP "delete=%s\n",
+					event->path,
+					event->file_info.info_data.file_info_disposition.delete_file ? "true" : "false");
 			break;
 		case FileEndOfFileInformation:
-			printf("setinfo: t=FileEndOfFileInformation end=%I64u \"%S\"\n",
-					event->file_info.info_data.file_info_end_of_file.end_of_file,
-					event->path);
+			printf("setinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileEndOfFileInformation" FIELD_SEP "end=%I64u\n",
+					event->path,
+					event->file_info.info_data.file_info_end_of_file.end_of_file);
 			break;
 		case FileLinkInformation:
-			printf("setinfo: t=FileLinkInformation replace=%s root=0x%x name=\"%S\" \"%S\"\n",
+			printf("setinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileLinkInformation" FIELD_SEP "replace=%s" FIELD_SEP "root=0x%x" FIELD_SEP "name=\"%S\"\n",
+					event->path,
 					event->file_info.info_data.file_info_link.replace_if_exists ? "true" : "false",
 					event->file_info.info_data.file_info_link.root_directory,
-					event->file_info.info_data.file_info_link.file_name,
-					event->path);
+					event->file_info.info_data.file_info_link.file_name);
 			break;
 		case FilePositionInformation:
-			printf("setinfo: t=FilePositionInformation pos=%I64u \"%S\"\n",
-					event->file_info.info_data.file_info_position.current_byte_offset,
-					event->path);
+			printf("setinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FilePositionInformation" FIELD_SEP "pos=%I64u\n",
+					event->path,
+					event->file_info.info_data.file_info_position.current_byte_offset);
 			break;
 		case FileRenameInformation:
-			printf("setinfo: t=FileRenameInformation replace=%s root=%x name=\"%S\" \"%S\"\n",
+			printf("setinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileRenameInformation" FIELD_SEP "replace=%s" FIELD_SEP "root=0x%x" FIELD_SEP "name=\"%S\"\n",
+					event->path,
 					event->file_info.info_data.file_info_rename.replace_if_exists ? "true" : "false",
 					event->file_info.info_data.file_info_rename.root_directory,
-					event->file_info.info_data.file_info_rename.file_name,
-					event->path);
+					event->file_info.info_data.file_info_rename.file_name);
 			break;
 		case FileValidDataLengthInformation:
-			printf("setinfo: t=FileValidDataLengthInformation len=%I64u \"%S\"\n",
-					event->file_info.info_data.file_info_valid_data_length.valid_data_length,
-					event->path);
+			printf("setinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=FileValidDataLengthInformation" FIELD_SEP "len=%I64u\n",
+					event->path,
+					event->file_info.info_data.file_info_valid_data_length.valid_data_length);
 			break;
 		default:
-			printf("setinfo: t=%d s=%d \"%S\"\n",
-					event->file_info.info_type, event->file_info.info_size, event->path);
+			printf("setinfo:" FIELD_SEP "\"%S\"" FIELD_SEP "t=%d" FIELD_SEP "s=%d\n",
+					event->path,
+					event->file_info.info_type,
+					event->file_info.info_size);
 		}
 		break;
 	case ET_REG_CLOSE:
-		printf("reg_close: %x \"%S\"\n", event->reg_close.handle, event->path);
+		printf("reg_close:" FIELD_SEP "\"%S\"" FIELD_SEP "0x%x\n",
+				event->path,
+				event->reg_close.handle);
 		break;
 	case ET_REG_CREATE:
-		printf("reg_create: hd=%x da=%x co=%x cd=%x \"%S\"\n", event->reg_create.handle, event->reg_create.desired_access, event->reg_create.create_options, event->reg_create.creation_disposition, event->path);
+		printf("reg_create:" FIELD_SEP "\"%S\"" FIELD_SEP "hd=0x%x" FIELD_SEP "da=0x%x" FIELD_SEP "co=0x%x" FIELD_SEP "cd=0x%x\n",
+				event->path,
+				event->reg_create.handle,
+				event->reg_create.desired_access,
+				event->reg_create.create_options,
+				event->reg_create.creation_disposition);
 		break;
 	case ET_REG_DELETE:
-		printf("reg_delete: %x \"%S\"\n", event->reg_delete.handle, event->path);
+		printf("reg_delete:" FIELD_SEP "\"%S\"" FIELD_SEP "0x%x\n",
+				event->path,
+				event->reg_delete.handle);
 		break;
 	case ET_REG_DELETEVALUE:
-		printf("reg_deletevalue: %x \"%S\"\n", event->reg_delete_value.handle, event->path);
+		printf("reg_deletevalue:" FIELD_SEP "\"%S\"" FIELD_SEP "0x%x\n",
+				event->path,
+				event->reg_delete_value.handle);
 		break;
 	case ET_REG_OPEN:
-		printf("reg_open: %x %x \"%S\"\n", event->reg_open.handle, event->reg_open.desired_access, event->path);
+		printf("reg_open:" FIELD_SEP "\"%S\"" FIELD_SEP "0x%x" FIELD_SEP "0x%x\n",
+				event->path,
+				event->reg_open.handle,
+				event->reg_open.desired_access);
 		break;
 	case ET_REG_QUERYVALUE:
 	case ET_REG_SETVALUE:
 		switch (event->reg_rw.value_type) {
 		case REG_BINARY:
-			printf("reg_%svalue: t=REG_BINARY l=%d \"%S\"\n",
+			printf("reg_%svalue:" FIELD_SEP "\"%S\"" FIELD_SEP "t=REG_BINARY" FIELD_SEP "l=%d\n",
 					event->type == ET_REG_QUERYVALUE ? "query" : "set",
-					event->reg_rw.value_length, event->path);
+					event->path,
+					event->reg_rw.value_length);
 			break;
 		case REG_DWORD:
-			printf("reg_%svalue: t=REG_DWORD v=%x \"%S\"\n",
+			printf("reg_%svalue:" FIELD_SEP "\"%S\"" FIELD_SEP "t=REG_DWORD" FIELD_SEP "v=0x%x\n",
 					event->type == ET_REG_QUERYVALUE ? "query" : "set",
-					*(unsigned int *)event->reg_rw.value, event->path);
+					event->path,
+					*(unsigned int *)event->reg_rw.value);
 			break;
 		case REG_EXPAND_SZ:
-			printf("reg_%svalue: t=REG_EXPAND_SZ l=%d v=\"%S\" \"%S\"\n",
+			printf("reg_%svalue:" FIELD_SEP "\"%S\"" FIELD_SEP "t=REG_EXPAND_SZ" FIELD_SEP "l=%d" FIELD_SEP "v=\"%S\"\n",
 					event->type == ET_REG_QUERYVALUE ? "query" : "set",
-					event->reg_rw.value_length, event->reg_rw.value, event->path);
+					event->path,
+					event->reg_rw.value_length,
+					event->reg_rw.value);
 			break;
 		case REG_SZ:
-			printf("reg_%svalue: t=REG_EXPAND_SZ l=%d v=\"%S\" \"%S\"\n",
+			printf("reg_%svalue:" FIELD_SEP "\"%S\"" FIELD_SEP "t=REG_EXPAND_SZ" FIELD_SEP "l=%d" FIELD_SEP "v=\"%S\"\n",
 					event->type == ET_REG_QUERYVALUE ? "query" : "set",
-					event->reg_rw.value_length, event->reg_rw.value, event->path);
+					event->path,
+					event->reg_rw.value_length,
+					event->reg_rw.value);
 			break;
 		default:
-			printf("reg_queryvalue: t=%x l=%d \"%S\"\n", event->reg_rw.value_type, event->reg_rw.value_length, event->path);
+			printf("reg_%svalue:" FIELD_SEP "\"%S\"" FIELD_SEP "t=0x%x" FIELD_SEP "l=%d\n",
+					event->type == ET_REG_QUERYVALUE ? "query" : "set",
+					event->path,
+					event->reg_rw.value_type,
+					event->reg_rw.value_length);
 		}
 		break;
 	case ET_PROC_PROC_CREATE:
-		printf("proc_create: ppid=%d, pid=%d\n", event->proc_proc_create.ppid, event->proc_proc_create.pid);
+		printf("proc_create:" FIELD_SEP "ppid=%d," FIELD_SEP "pid=%d\n",
+				event->proc_proc_create.ppid,
+				event->proc_proc_create.pid);
 		break;
 	case ET_PROC_PROC_TERM:
-		printf("proc_term: ppid=%d, pid=%d\n", event->proc_proc_term.ppid, event->proc_proc_term.pid);
+		printf("proc_term:" FIELD_SEP "ppid=%d," FIELD_SEP "pid=%d\n",
+				event->proc_proc_term.ppid,
+				event->proc_proc_term.pid);
 		break;
 	case ET_PROC_THREAD_CREATE:
-		printf("thread_create: tid=%d\n", event->proc_thread_create.tid);
+		printf("thread_create:" FIELD_SEP "tid=%d\n",
+				event->proc_thread_create.tid);
 		break;
 	case ET_PROC_THREAD_TERM:
-		printf("thread_term: tid=%d\n", event->proc_thread_create.tid);
+		printf("thread_term:" FIELD_SEP "tid=%d\n",
+				event->proc_thread_create.tid);
 		break;
 	case ET_PROC_IMAGE:
-		printf("image: %d %08x %d \"%S\"\n", event->proc_image.system, event->proc_image.base, event->proc_image.size, event->path);
+		printf("image:" FIELD_SEP "\"%S\"" FIELD_SEP "%d" FIELD_SEP "0x%08x" FIELD_SEP "%d\n",
+				event->path,
+				event->proc_image.system,
+				event->proc_image.base,
+				event->proc_image.size);
 		break;
 	default:
 		printf("unknown event\n");
